e16d18e4c630647e41a3f663e33e2640aa26a40f
Joe Dumford <dumford.joe@gmail.com>
2019-08-01 17:48:46 -0400

    Working in app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,27 @@
 //Scrape Button
+$("#scrapeButton").on("click", (e) => {
+    e.preventDefault();
+    $.get("/articles").done((data) => {
+        bb(data);
+    }); 
+});
+
+let listContainer = $('#myArticles');
+
+const bb = (data) => {
+    let listItems = xx(data);
+    listItems.forEach(item => {
+        console.log(item);
+        listContainer.append(item);
+    });
+};
+
+let xx = (data) => {
+    let arr = [];
+    data.forEach((item) => {
+        arr.push(`<li><span>${item.title}</span><span>${item.link}</span></li>`);
+    });
+    console.log(arr);
+    return arr;
+}
+
